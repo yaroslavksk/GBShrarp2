13d57eac1589cbf822fa391d58720b3e39dafad4
yaroslav_ksk <yaroslav_ksk@mail.ru>
2023-10-24 13:54:29 +0500

    Добавьте файлы проекта.

---
+++ IbitsT.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBShrarp2
+{
+    internal class IbitsT
+    {
+        public interface IBits
+        {
+            bool GetBit(int i);
+            void SetBit(bool bit, int index);
+        }
+
+
+        public class Bits : IBits
+        {
+            public Bits(byte b)
+            {
+                this.v = b;
+                MaxBitsCount = sizeof(byte) * 8;
+            }
+
+            public Bits(short b)
+            {
+                this.v = b;
+                MaxBitsCount = sizeof(short) * 8;
+            }
+
+            public Bits(int b)
+            {
+                this.v = b;
+                MaxBitsCount = sizeof(int) * 8;
+            }
+
+            public Bits(long b)
+            {
+                this.v = b;
+                MaxBitsCount = sizeof(long) * 8;
+            }
+
+            public long v { get; set; } = 0;
+            private int MaxBitsCount { get; set; }
+
+
+
+            public bool GetBit(int index)
+            {
+                if (index > MaxBitsCount || index < 0)
+                {
+                    Console.WriteLine($"Выход за пределы от 0 до {MaxBitsCount}");
+                    return false;
+                }
+
+                return ((v >> index) & 1) == 1;
+            }
+
+            public void SetBit(bool bit, int index)
+            {
+                if (index > MaxBitsCount || index < 0)
+                {
+                    Console.WriteLine($"Выход за пределы от 0 до {MaxBitsCount}");
+                    return;
+                }
+                if (bit == true)
+                    v = v | (1 << index);
+                else
+                {
+                    var mask = 1 << index;
+                    mask = 0xff ^ mask;
+                    v &= v & mask;
+                }
+            }
+
+            public static implicit operator Bits(int v)
+            {
+                try
+                {
+                    int a = v;
+                    return new Bits(v);
+                }
+                catch (StackOverflowException e) { throw new NotImplementedException(); } 
+            }
+            public static implicit operator Bits(long v)
+            {
+                try
+                {
+                    long a = v;
+                    return new Bits(v);
+                }
+                catch (StackOverflowException e) { throw new NotImplementedException(); }
+            }
+            public static implicit operator Bits(byte v)
+            {
+                try
+                {
+                    byte a = v;
+                    return new Bits(v);
+                }
+                catch (StackOverflowException e) { throw new NotImplementedException(); }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static GBShrarp2.IbitsT;
+
+namespace GBShrarp2
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            long a = 123123123;
+            int b = 0;
+            byte c = 1;
+            Bits bits = c;
+            Console.WriteLine(bits.ToString());
+        }
+    }
+}
